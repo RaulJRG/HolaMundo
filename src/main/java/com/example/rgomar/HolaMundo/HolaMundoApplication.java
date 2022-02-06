bb96efcf883b3af28e4abba32eead933f4fe4a75
RaulJRG <raulgomar.12nv@gmail.com>
2022-02-05 18:37:16 -0600

    Commit prueba

--- src/main/java/com/example/rgomar/HolaMundo/HolaMundoApplication.java
+++ src/main/java/com/example/rgomar/HolaMundo/HolaMundoApplication.java
@@ -3,7 +3,7 @@ package com.example.rgomar.HolaMundo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class HolaMundoApplication {
 
 	public static void main(String[] args) {
